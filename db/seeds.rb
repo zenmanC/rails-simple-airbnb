69b2bfaa44eed49aeaec285f42e9c5f61216ce7d
Chris Cheung <ckwcheung123@icloud.com>
2018-11-14 21:40:52 +0800

    routes established db seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Clearing Database"
+Flat.destroy_all
+
+puts "Generating Fake Database"
+
+price_array = [50, 100, 75, 150]
+guests_array = [1, 2, 3]
+
+6.times do
+  Flat.create!(
+  name: Faker::Book.title,
+  address: Faker::Address.city,
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: price_array.sample,
+  number_of_guests: guests_array.sample
+)
+end
+
+puts "DB Created"
