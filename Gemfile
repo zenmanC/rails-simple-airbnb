68b1630f2cf41242ccbd1899b3cfa61db0ac5a7b
Chris Cheung <ckwcheung123@icloud.com>
2018-11-14 22:35:07 +0800

    first steps with air b

--- Gemfile
+++ Gemfile
@@ -3,6 +3,14 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.4.4'
 
+gem 'simple_form'
+
+gem 'bootstrap-sass'
+
+gem 'font-awesome-sass'
+
+gem 'simple_form'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.1'
 # Use sqlite3 as the database for Active Record
--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,2 +1,29 @@
 class FlatsController < ApplicationController
+
+  def index
+    @flats = Flat.all
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to flat_path(@flat)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @flat = Flat.find(params[:id])
+  end
+
+private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
 end
